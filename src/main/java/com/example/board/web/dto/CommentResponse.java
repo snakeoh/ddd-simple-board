428d99a3e09e27a6eb2a96bc992f7a1156fb7e3a
Damon <dh.oh@dtonic.io>
2025-07-02 17:46:30 +0900

    comment response

--- src/main/java/com/example/board/web/dto/CommentResponse.java
+++ src/main/java/com/example/board/web/dto/CommentResponse.java
@@ -3,6 +3,8 @@ package com.example.board.web.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.example.board.domain.entity.Comment;
+
 import lombok.Builder;
 
 @Builder
@@ -11,4 +13,23 @@ public record CommentResponse(
         String authorName,
         String content,
         LocalDateTime createdAt) {
+
+    // 두 가지 방법이 있다.
+    // 1. Builder 패턴을 사용하여 생성
+    // public static CommentResponse from(Comment comment) {
+    // return CommentResponse.builder()
+    // .commentId(comment.getCommentId())
+    // .authorName(comment.getAuthorName().getValue())
+    // .content(comment.getContent())
+    // .createdAt(comment.getCreatedAt())
+    // .build();
+    // }
+    // 2. 생성자를 사용하여 생성
+    public static CommentResponse from(Comment comment) {
+        return new CommentResponse(
+                comment.getCommentId(),
+                comment.getAuthorName().getValue(),
+                comment.getContent(),
+                comment.getCreatedAt());
+    }
 }
