a57d6dc38b10c68955a1992d589a1da5dc722f67
Damon <dh.oh@dtonic.io>
2025-07-04 10:14:47 +0900

    글 유효성 검증

--- src/main/java/com/example/board/web/dto/CreateCommentRequest.java
+++ src/main/java/com/example/board/web/dto/CreateCommentRequest.java
@@ -1,7 +1,11 @@
 package com.example.board.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
-public record CreateCommentRequest(String authorName, String content) {
+public record CreateCommentRequest(
+        @NotBlank(message = "작성자 이름은 비어있을 수 없습니다.") String authorName,
+
+        String content) {
 }
--- src/main/java/com/example/board/web/dto/CreatePostRequest.java
+++ src/main/java/com/example/board/web/dto/CreatePostRequest.java
@@ -1,7 +1,7 @@
 package com.example.board.web.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CreatePostRequest {
     @Schema(description = "작성자 이름", example = "홍길동")
+    @NotBlank(message = "작성자 이름은 비어있을 수 없습니다.")
     private String authorName;
 
     private String title;
