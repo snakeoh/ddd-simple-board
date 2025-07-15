16818348079efe74396b4ef1e2ba607d684fdd06
Damon <dh.oh@dtonic.io>
2025-07-15 14:13:08 +0900

    import.sql 실행 시 디코딩 에러

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,12 +2,21 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@PostConstruct
+	public void logEncoding() {
+		System.out.println("### JVM Encoding: " + System.getProperty("file.encoding"));
 	}
 
 }
